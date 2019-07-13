f960dbf3a74dd9e67bc052ef1552af6a5c543355
nileshpardeshi <nileshpardeshi2507@gmail.com>
2019-07-14 00:35:58 +0530

    try2

--- src/facebookEgit/login.java
+++ src/facebookEgit/login.java
@@ -6,6 +6,7 @@ public class login {
 		// TODO Auto-generated method stub
 		System.out.println("Hello by dev1");
 		System.out.println("changes for UPI");
+		System.out.println("Try2");
 		System.out.println("conflict");
 
 	}
