54e51e768625e41ff9bed2da2739fbdc80214b08
nileshpardeshi <43164375+nileshpardeshi@users.noreply.github.com>
2019-07-14 00:25:03 +0530

    conflict try

--- src/facebookEgit/login.java
+++ src/facebookEgit/login.java
@@ -5,7 +5,8 @@ public class login {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello by dev1");
-		System.out.println("changes for UPI brach by dev 2");
+		System.out.println("changes for UPI");
+		System.out.println("conflict");
 
 	}
 
